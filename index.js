358a81fe5c97ed322c5f2dbd1b4534db27dff0c9
nengraf123 <nengraf123@gmail.com>
2024-06-13 13:01:52 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+function handleTabClicked(event) {
+    const tabCategory = event.target.id;
+    const content = document.querySelector(`.content.${tabCategory}`);
+    const contents = document.querySelectorAll(`.content`);
+    for (let current of contents) {
+        if (current === content) {
+            current.classList.add("active");
+        } else {
+            current.classList.remove("active");
+        }
+    }
+    console.log(content)
+}
